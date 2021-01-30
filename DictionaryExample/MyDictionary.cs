fe21a985b6c9b6cfe10f6763343adc9c53311896
SEFA KORKMAZ <sefakjk4017@gmail.com>
2021-01-30 03:18:20 +0300

    Proje dosyası ekle.

---
+++ DictionaryExample/MyDictionary.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<T, V>
+
+    {
+        T[] keyArray;
+        V[] valueArray;
+
+        public MyDictionary()
+        {
+            keyArray = new T[0];
+            valueArray = new V[0];
+        }
+
+
+        public void Add(T key, V value)
+        {
+
+            T[] tempKey = keyArray;
+            V[] tempValue = valueArray;
+
+            keyArray = new T[keyArray.Length + 1];
+
+            valueArray = new V[valueArray.Length + 1];
+
+            for (int i = 0; i < tempKey.Length; i++)
+
+            {
+                keyArray[i] = tempKey[i];
+
+            }
+
+            for (int i = 0; i < tempValue.Length; i++)
+            {
+                valueArray[i] = tempValue[i];
+            }
+
+            keyArray[keyArray.Length - 1] = key;
+
+            valueArray[valueArray.Length - 1] = value;
+
+            Console.WriteLine(key + " Model araba : " + " " + value+" aittir");
+        }
+
+
+    }
+
+
+}
---
+++ DictionaryExample/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, string> araba = new MyDictionary<string, string>();
+            araba.Add("Clio","Renault");
+            araba.Add("Astra", "Opel");
+            araba.Add("Focus", "Ford");
+        }
+    }
+}
